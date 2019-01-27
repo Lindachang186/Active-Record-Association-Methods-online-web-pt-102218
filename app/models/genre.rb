c6d32b9b4e45ed8be4b0aad475ab541a699ee4a7
Linda Chang <lindachang186@gmail.com>
2019-01-27 19:30:01 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,6 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     self.artists.map do |a|
-      binding.pry
       a.name
     end
   end
